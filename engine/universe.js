5a769bf0ba31558da371296a419fc540b7f9e14c
msdestefano <markdestefano@hotmail.com>
2026-03-15 10:20:37 -0400

    Create universe.js

---
+++ engine/universe.js
@@ -0,0 +1,11 @@
+const UNIVERSE = [
+  { symbol: "AAPL", index: "NDX" },
+  { symbol: "MSFT", index: "NDX" },
+  { symbol: "SPY", index: "SPX" },
+  { symbol: "QQQ", index: "NDX" },
+  { symbol: "DIA", index: "DJI" }
+];
+
+module.exports = {
+  UNIVERSE
+};
